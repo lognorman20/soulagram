b5b7d7c706c9c75ec6a8c29a6b9ce7a7f55d0802
Logan Norman <logannormanss@gmail.com>
2022-06-14 16:58:32 -0700

    implemented time since text view

--- app/src/main/java/com/example/instagram/PostDetailsActivity.java
+++ app/src/main/java/com/example/instagram/PostDetailsActivity.java
@@ -1,6 +1,7 @@
 package com.example.instagram;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -9,6 +10,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.bumptech.glide.Glide;
 
 import org.parceler.Parcels;
+
+import java.util.Date;
 
 public class PostDetailsActivity extends AppCompatActivity {
     private Post post;
@@ -31,7 +34,43 @@ public class PostDetailsActivity extends AppCompatActivity {
         post = (Post) Parcels.unwrap(getIntent().getParcelableExtra("post"));
         tvDetailedDescription.setText(post.getDescription());
         tvDetailedUsername.setText(post.getUsername());
-        tvCreatedAt.setText(post.getCreatedAt().toString());
+        tvCreatedAt.setText(calculateTimeAgo(post.getCreatedAt()));
         Glide.with(PostDetailsActivity.this).load(post.getImage().getUrl()).centerCrop().into(ivDetailedPostImage);
+    }
+
+    public static String calculateTimeAgo(Date createdAt) {
+
+        int SECOND_MILLIS = 1000;
+        int MINUTE_MILLIS = 60 * SECOND_MILLIS;
+        int HOUR_MILLIS = 60 * MINUTE_MILLIS;
+        int DAY_MILLIS = 24 * HOUR_MILLIS;
+
+        try {
+            createdAt.getTime();
+            long time = createdAt.getTime();
+            long now = System.currentTimeMillis();
+
+            final long diff = now - time;
+            if (diff < MINUTE_MILLIS) {
+                return "just now";
+            } else if (diff < 2 * MINUTE_MILLIS) {
+                return "a minute ago";
+            } else if (diff < 50 * MINUTE_MILLIS) {
+                return diff / MINUTE_MILLIS + " m";
+            } else if (diff < 90 * MINUTE_MILLIS) {
+                return "an hour ago";
+            } else if (diff < 24 * HOUR_MILLIS) {
+                return diff / HOUR_MILLIS + " h";
+            } else if (diff < 48 * HOUR_MILLIS) {
+                return "yesterday";
+            } else {
+                return diff / DAY_MILLIS + " d";
+            }
+        } catch (Exception e) {
+            Log.i("Error:", "getRelativeTimeAgo failed", e);
+            e.printStackTrace();
+        }
+
+        return "";
     }
 }
