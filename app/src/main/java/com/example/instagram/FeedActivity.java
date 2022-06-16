231226f4850cde0371c76be65fbca70a4e4650fa
Logan Norman <logannormanss@gmail.com>
2022-06-16 15:49:26 -0700

    finished endless scroll view

--- app/src/main/java/com/example/instagram/FeedActivity.java
+++ app/src/main/java/com/example/instagram/FeedActivity.java
@@ -22,11 +22,54 @@ public class FeedActivity extends AppCompatActivity {
     protected PostsAdapter adapter;
     protected List<Post> allPosts;
     private SwipeRefreshLayout swipeContainer;
+    // Store a member variable for the listener
+    private EndlessRecyclerViewScrollListener scrollListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+
+        rvPosts = (RecyclerView) findViewById(R.id.rvPosts);
+        allPosts = new ArrayList<>();
+        adapter = new PostsAdapter(this, allPosts);
+        rvPosts.setAdapter(adapter);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+
+
+        scrollListener = new EndlessRecyclerViewScrollListener(linearLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                // Triggered only when new data needs to be appended to the list
+                // Add whatever code is needed to append new items to the bottom of the list
+                // clear the array of data
+                queryPosts(allPosts.size());
+            }
+        };
+
+
+        rvPosts.setLayoutManager(linearLayoutManager);
+        rvPosts.addOnScrollListener(scrollListener);
+
+
+
+        // Configure the RecyclerView
+
+
+        // Retain an instance so that you can call `resetState()` for fresh searches
+
+
+        // Adds the scroll listener to RecyclerView
+
+
+        // initialize the array that will hold posts and create a PostsAdapter
+
+        // set the adapter on the recycler view
+
+        // set the layout manager on the recycler view
+       // rvPosts.setLayoutManager(new LinearLayoutManager(this));
+        // query posts from Parstagram
+        queryPosts(0);
 
         swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
         // Setup refresh listener which triggers new data loading
@@ -36,7 +79,8 @@ public class FeedActivity extends AppCompatActivity {
                 // Your code to refresh the list here.
                 // Make sure you call swipeContainer.setRefreshing(false)
                 // once the network request has completed successfully.
-                fetchTimelineAsync(0);
+                adapter.clear();
+                queryPosts(0);
             }
         });
 
@@ -45,35 +89,16 @@ public class FeedActivity extends AppCompatActivity {
                 android.R.color.holo_green_light,
                 android.R.color.holo_orange_light,
                 android.R.color.holo_red_light);
-
-        rvPosts = findViewById(R.id.rvPosts);
-        // initialize the array that will hold posts and create a PostsAdapter
-        allPosts = new ArrayList<>();
-        adapter = new PostsAdapter(this, allPosts);
-        // set the adapter on the recycler view
-        rvPosts.setAdapter(adapter);
-        // set the layout manager on the recycler view
-        rvPosts.setLayoutManager(new LinearLayoutManager(this));
-        // query posts from Parstagram
-        queryPosts();
     }
 
-    private void fetchTimelineAsync(int i) {
-        // Remember to CLEAR OUT old items before appending in the new ones
-        adapter.clear();
-        // ...the data has come back, add new items to your adapter...
-        queryPosts();
-        // Now we call setRefreshing(false) to signal refresh has finished
-        swipeContainer.setRefreshing(false);
-    }
-
-    private void queryPosts() {
+    private void queryPosts(int skipAmount) {
         // specify what type of data we want to query - Post.class
         ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
         // include data referred by user key
         query.include(Post.KEY_USER);
         // limit query to latest 20 items
-        query.setLimit(20);
+        query.setLimit(2);
+        query.setSkip(skipAmount);
         // order posts by creation date (newest first)
         query.addDescendingOrder("createdAt");
         // start an asynchronous call for posts
@@ -93,6 +118,7 @@ public class FeedActivity extends AppCompatActivity {
 
                 // save received posts to list and notify adapter of new data
                 allPosts.addAll(posts);
+                swipeContainer.setRefreshing(false);
                 adapter.notifyDataSetChanged();
             }
         });
---
+++ app/src/main/java/com/example/instagram/SignupActivity.java
@@ -0,0 +1,7 @@
+package com.example.instagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignupActivity extends AppCompatActivity {
+
+}
